5e9287ea963fa5512ef48b9e9edfb98c6ac75f1e
limtae <dkslsdkdl@naver.com>
2021-05-02 17:37:00 +0900

    create server

---
+++ server.js
@@ -0,0 +1,21 @@
+
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+if(process.env.NODE_ENV === "development") {
+    app.use(cors({
+        origin: process.env.CLIENT_URL
+    }));
+
+    app.use(morgan('dev'));
+}
+
+const port = process.env.PORT;
+app.listen(port, () => console.log(`server running on port${port}`))
